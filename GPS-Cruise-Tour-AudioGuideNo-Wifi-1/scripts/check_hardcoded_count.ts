ffcb64cd285962bf8e065cdeff10997d31c52d65
maibauntourph-collab <maibauntourph@gmail.com>
2026-02-18 23:01:43 +0800

    Delete GPS-Cruise-Tour-AudioGuideNo-Wifi-1 directory

--- GPS-Cruise-Tour-AudioGuideNo-Wifi-1/scripts/check_hardcoded_count.ts
+++
@@ -1,5 +0,0 @@
-import { LANDMARKS } from "../server/data/landmarks";
-import { CITIES } from "../server/data/cities";
-
-console.log(`Hardcoded LANDMARKS count: ${LANDMARKS.length}`);
-console.log(`Hardcoded CITIES count: ${CITIES.length}`);
