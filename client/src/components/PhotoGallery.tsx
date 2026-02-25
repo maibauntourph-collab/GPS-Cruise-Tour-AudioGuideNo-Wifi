7a3056b9b9f74be7e650753adb3684e9fe4ddd0b
maibauntourph <maibauntourph@gmail.com>
2026-02-25 23:46:06 +0800

    chore: final design polish and stability check for LandmarkPanel and PhotoGallery [2026-02-25 23:48]

--- client/src/components/PhotoGallery.tsx
+++ client/src/components/PhotoGallery.tsx
@@ -158,12 +158,12 @@ export default function PhotoGallery({ photos, title }: PhotoGalleryProps) {
                     initial={{ opacity: 0, x: 100, scale: 0.9 }}
                     animate={{ opacity: 1, x: 0, scale: 1 }}
                     exit={{ opacity: 0, x: -100, scale: 0.9 }}
-                    transition={{ type: "spring", stiffness: 300, damping: 30 }}
+                    transition={{ type: "spring", stiffness: 400, damping: 40, mass: 1 }}
                     drag="x"
                     dragConstraints={{ left: 0, right: 0 }}
-                    dragElastic={0.7}
+                    dragElastic={0.5}
                     onDragEnd={(_, info) => {
-                      const swipeThreshold = 50;
+                      const swipeThreshold = 30; // 감도 향상을 위해 임계값 하향 조정
                       if (info.offset.x < -swipeThreshold) handleNext();
                       else if (info.offset.x > swipeThreshold) handlePrevious();
                     }}
